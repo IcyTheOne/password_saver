1e3d4d903198e996d8ec4db137986214d1e978ca
IcyTheOne <icybusinessicy@outlook.com>
2020-12-04 17:29:26 +0100

    Added the password reset functions and pages. Try it out with your user
    Furthermore an email is now associated with the app. (Gmail).

--- password_saver/urls.py
+++ password_saver/urls.py
@@ -23,8 +23,32 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #users app urls
     path('users/', include('users.urls', namespace='users')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', user_views.register, name='register'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
+
+    #vault app urls
     path('vault/', include('vault.urls')),
 ]
