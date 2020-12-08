c168b44f8bfbd2dfb3940cc731b7205f0dd6f3d2
elenagudzh <44157910+elenagudzh@users.noreply.github.com>
2020-12-08 14:15:06 +0100

    Links between different pages, also some button editing

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()  # aaand this is how new user is added to DB
             username = form.cleaned_data.get('username')
             messages.success(request,
-                             f'Account created for {username}!')  # gotta fix this cause its fucking up the view
+                             f'Account created for {username}!')  # message showing the user was created
             return redirect('vault-home')
     else:
         form = UserRegisterForm()
