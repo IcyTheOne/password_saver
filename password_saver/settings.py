9fdc2ef3b980b9e395b6c8c5684cee74eacb5714
elenagudzh <44157910+elenagudzh@users.noreply.github.com>
2020-12-19 16:41:30 +0100

    secretkey

--- password_saver/settings.py
+++ password_saver/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '@kq%^rkpnrm3n8o@n-p$5uevphw43z=zsi81pw**$d%!41znl^'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
 
 ALLOWED_HOSTS = ['passmansaver.herokuapp.com']
 # Application definition
