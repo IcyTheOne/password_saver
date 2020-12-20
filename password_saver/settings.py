ec69f34b9fd30daf5987282c64ea98c95fc42199
IcyTheOne <icybusinessicy@outlook.com>
2020-12-20 16:16:39 +0100

    give a+ lgtm

--- password_saver/settings.py
+++ password_saver/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Test to get latest test from LGTM
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '@kq%^rkpnrm3n8o@n-p$5uevphw43z=zsi81pw**$d%!41znl^'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
